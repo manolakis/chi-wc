9b1f49f2872376cb36941882e623069f2e7d79d1
Manuel Martin <manuel.martin@gmail.com>
2020-09-13 23:55:51 +0200

    feat: add color design tokens

---
+++ packages/style/colors.js
@@ -0,0 +1,45 @@
+import { css } from '@lion/core';
+
+export const primaryAccent = css`var(--primary-accent, #0047BB)`;
+export const secondaryAccent = css`var(--secondary-accent, #48D597)`;
+
+export const corporateContent = css`var(--corporate-content, #000000)`;
+export const primaryText = css`var(--primary-text, #242526)`;
+export const secondaryText = css`var(--secondary-text, #53565A)`;
+export const disabledText = css`var(--disabled-text, #72767A)`;
+export const disabledIcon = css`var(--disabled-icon, var(--disabled-text))`;
+
+export const highContrastBorder = css`var(--high-contrast-border, #ACB0B5)`; // apply filter?
+export const mediumContrastBorder = css`var(--medium-contrast-border, #D0D4D9)`; // apply filter?
+export const lowContrastBorder = css`var(--low-contrast-border, #EDF0F2)`; // apply filter?
+
+export const lowContrastBackground = css`var(--low-contrast-background, #F8F9F9)`;
+export const bodyBackground = css`var(--body-background, #FFFFFF)`;
+
+export const successText = css`var(--success-text, #007E46)`;
+export const successBorder = css`var(--success-border, #009E5C)`;
+export const successLowContrastBorder = css`var(--success-low-contrast-border, #ABE0C9)`;
+export const successIcon = css`var(--success-icon, var(--success-border))`;
+export const successMediumContrastBackground = css`var(--success-medium-contrast-background, #DAF5E9)`; // apply filter?
+export const successLowContrastBackground = css`var(--success-low-contrast-background, #EDFAF3)`; // apply filter?
+
+export const infoText = css`var(--info-text, #0060FA)`;
+export const infoBorder = css`var(--info-border, #2580FF)`;
+export const infoLowContrastBorder = css`var(--info-low-contrast-border, #C0D7FA)`;
+export const infoIcon = css`var(--info-icon, var(--info-border))`;
+export const infoMediumContrastBackground = css`var(--info-medium-contrast-background, #E6F0FF)`; // apply filter?
+export const infoLowContrastBackground = css`var(--info-low-contrast-background, #F5F8FC)`; // apply filter?
+
+export const dangerText = css`var(--danger-text, #D81820)`;
+export const dangerBorder = css`var(--danger-border, #F0363C)`;
+export const dangerLowContrastBorder = css`var(--danger-low-contrast-border, #FCC7C9)`;
+export const dangerIcon = css`var(--danger-icon, var(--danger-border))`;
+export const dangerMediumContrastBackground = css`var(--danger-medium-contrast-background, #FCE8E9)`; // apply filter?
+export const dangerLowContrastBackground = css`var(--danger-low-contrast-background, #FFF5F5)`; // apply filter?
+
+export const warningText = css`var(--warning-text, #A15C00)`;
+export const warningBorder = css`var(--warning-border, #CA7300)`;
+export const warningLowContrastBorder = css`var(--warning-low-contrast-border, #F5CC93)`;
+export const warningIcon = css`var(--warning-icon, var(--warning-border))`;
+export const warningMediumContrastBackground = css`var(--warning-medium-contrast-background, #FAECD9)`; // apply filter?
+export const warningLowContrastBackground = css`var(--warning-low-contrast-background, #FFF8ED)`; // apply filter?

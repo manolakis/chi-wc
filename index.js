7587c182ffcd9bdd774a8e9e8386ec3f2319e8a3
Manuel Martin <manuel.martin@gmail.com>
2020-09-18 20:18:34 +0200

    feat: export basic @lion components

---
+++ index.js
@@ -0,0 +1,105 @@
+// *** core *****************************************************************************
+export {
+  // lit-html
+  html,
+  svg,
+  render,
+  noChange,
+  nothing,
+  directive,
+  isDirective,
+  TemplateResult,
+  SVGTemplateResult,
+  renderShady,
+  asyncAppend,
+  asyncReplace,
+  cache,
+  classMap,
+  guard,
+  ifDefined,
+  repeat,
+  styleMap,
+  unsafeHTML,
+  until,
+  NodePart,
+  // lit-element
+  LitElement,
+  supportsAdoptingStyleSheets,
+  CSSResult,
+  unsafeCSS,
+  css,
+  defaultConverter,
+  notEqual,
+  UpdatingElement,
+  customElement,
+  property,
+  query,
+  queryAll,
+  eventOptions,
+  // open-wc
+  ScopedElementsMixin,
+  // lion
+  dedupeMixin,
+  DelegateMixin,
+  SlotMixin,
+  UpdateStylesMixin,
+} from '@lion/core';
+
+// *** ajax ******************************************************************************
+export { ajax, setAjax, AjaxClass } from '@lion/ajax';
+
+// *** forms *****************************************************************************
+export {
+  Required,
+  IsString,
+  EqualsLength,
+  MinLength,
+  MaxLength,
+  MinMaxLength,
+  IsEmail,
+  Pattern,
+  IsNumber,
+  MinNumber,
+  MaxNumber,
+  MinMaxNumber,
+  IsDate,
+  MinDate,
+  MaxDate,
+  MinMaxDate,
+  IsDateDisabled,
+} from '@lion/form-core';
+
+export { Unparseable, Validator } from '@lion/form-core';
+export { loadDefaultFeedbackMessages } from '@lion/validate-messages';
+
+// *** localize ***********************************************************************************
+export {
+  localize,
+  LocalizeManager,
+  LocalizeMixin,
+  // formatting
+  formatNumber,
+  formatNumberToParts,
+  getFractionDigits,
+  getGroupSeparator,
+  getDecimalSeparator,
+  normalizeDateTime,
+  formatDate,
+  parseDate,
+  getDateFormatBasedOnLocale,
+  // names
+  getMonthNames,
+  getWeekdayNames,
+} from '@lion/localize';
+
+// *** overlays ***********************************************************************************
+export {
+  overlays,
+  setOverlays,
+  OverlaysManager,
+  OverlayController,
+  OverlayMixin,
+  withBottomSheetConfig,
+  withDropdownConfig,
+  withModalDialogConfig,
+} from '@lion/overlays';

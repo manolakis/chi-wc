ba255239f76128a755485d1e3d764a068029601c
Marcos Gil <marcos90gil@gmail.com>
2020-09-30 10:10:01 +0200

    feat(header): add extra content

--- packages/header/docs/01-headerDemo.js
+++ packages/header/docs/01-headerDemo.js
@@ -1,7 +1,23 @@
 import { html } from '@lion/core';
 import { ChiHeader } from '../ChiHeader.js';
+import { ChiButton } from '../../button/ChiButton.js';
+
+if (!customElements.get('chi-button')) {
+  customElements.define('chi-button', ChiButton);
+}
 
 customElements.define('chi-header', ChiHeader);
 
 export const renderHeader = () =>
   html` <chi-header application-title="Application Title"></chi-header> `;
+
+export const renderHeaderExtra = () =>
+  html`
+    <chi-header application-title="Application Title">
+      <a href="#" slot="header-left">Left</a>
+      <div slot="header-right">
+        <a href="#">Right</a>
+        <chi-button>Right</chi-button>
+      </div>
+    </chi-header>
+  `;
